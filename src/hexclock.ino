b9dbd3323e1d4e2cc223bb9a5723fec9416a4b1a
littleaj <1690572+littleaj@users.noreply.github.com>
2020-12-28 17:56:01 -0800

    refactor clock display into separate class. lower initial intensity.

--- src/hexclock.ino
+++ src/hexclock.ino
@@ -1,13 +1,15 @@
 #include <Wire.h>
 #include <RtcDS3231.h>
-#include <LedControl.h>
+// #include <LedControl.h>
+
+#include "ClockDisplay.h"
 
 #define CS_HOUR 2
 #define CS_MINUTE16 1
 #define CS_MINUTE1 0
 
-LedControl lc = LedControl(12,11,10,3);
 RtcDS3231<TwoWire> rtc(Wire);
+ClockDisplay disp = ClockDisplay(12, 11, 10);
 
 void setup() {
     // unused pins
@@ -54,214 +56,14 @@ void setup() {
     rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
     rtc.Enable32kHzPin(false);
 
-    lc.shutdown(0, true);
-    lc.shutdown(1, true);
-    lc.shutdown(2, true);
-    delay(1000);
-    lc.shutdown(0, false);
-    lc.shutdown(1, false);
-    lc.shutdown(2, false);
+    disp.begin();
 
-    lc.setIntensity(0, 8);
-    lc.setIntensity(1, 8);
-    lc.setIntensity(2, 8);
-
-    lc.clearDisplay(0);
-    lc.clearDisplay(1);
-    lc.clearDisplay(2);
     Serial.println("setup done");
 }
 
 
 // delay time between faces
 unsigned long delaytime=1000;
-
-uint8_t zero[8] = {
-    B00111100,
-    B01000010,
-    B01000010,
-    B01000010,
-    B01000010,
-    B01000010,
-    B01000010,
-    B00111100
-};
-
-uint8_t one[8] = {
-    B00011000,
-    B00111000,
-    B01011000,
-    B00011000,
-    B00011000,
-    B00011000,
-    B00011000,
-    B01111110
-};
-
-uint8_t two[8] = {
-    B00111100,
-    B01000010,
-    B00000010,
-    B00000100,
-    B00001000,
-    B00010000,
-    B00100000,
-    B01111110
-};
-
-uint8_t three[8] = {
-    B00111100,
-    B01000010,
-    B00000010,
-    B00011100,
-    B00000010,
-    B00000010,
-    B01000010,
-    B00111100
-};
-
-uint8_t four[8] = {
-    B00001100,
-    B00010100,
-    B00100100,
-    B01000100,
-    B01111110,
-    B00000100,
-    B00000100,
-    B00000100
-};
-
-uint8_t five[8] = {
-    B01111110,
-    B01000000,
-    B01000000,
-    B01111100,
-    B00000010,
-    B00000010,
-    B01000010,
-    B00111100
-};
-
-uint8_t six[8] = {
-    B00111100,
-    B01000010,
-    B01000000,
-    B01111100,
-    B01000010,
-    B01000010,
-    B01000010,
-    B00111100
-};
-
-uint8_t seven[8] = {
-    B01111110,
-    B01000010,
-    B00000110,
-    B00001100,
-    B00011000,
-    B00110000,
-    B01100000,
-    B01000000
-};
-
-uint8_t eight[8] = {
-    B00111100,
-    B01000010,
-    B01000010,
-    B00111100,
-    B01000010,
-    B01000010,
-    B01000010,
-    B00111100
-};
-
-uint8_t nine[8] = {
-    B00111100,
-    B01000010,
-    B01000010,
-    B00111100,
-    B00000100,
-    B00001000,
-    B00010000,
-    B00100000
-};
-
-uint8_t ten[8] = {
-    B00011000,
-    B00100100,
-    B01000010,
-    B01000010,
-    B01111110,
-    B01000010,
-    B01000010,
-    B01000010
-};
-
-uint8_t eleven[8] = {
-    B01111100,
-    B01000010,
-    B01000010,
-    B01111100,
-    B01000010,
-    B01000010,
-    B01000010,
-    B01111100
-};
-
-uint8_t twelve[8] = {
-    B00111100,
-    B01000010,
-    B01000000,
-    B01000000,
-    B01000000,
-    B01000000,
-    B01000010,
-    B00111100
-};
-
-uint8_t thirteen[8] = {
-    B01111000,
-    B01000100,
-    B01000010,
-    B01000010,
-    B01000010,
-    B01000010,
-    B01000100,
-    B01111000
-};
-
-uint8_t fourteen[8] = {
-    B01111110,
-    B01000000,
-    B01000000,
-    B01111100,
-    B01000000,
-    B01000000,
-    B01000000,
-    B01111110
-};
-
-uint8_t fifteen[8] = {
-    B01111110,
-    B01000000,
-    B01000000,
-    B01111100,
-    B01000000,
-    B01000000,
-    B01000000,
-    B01000000
-};
-
-void draw(int addr, uint8_t * pixels) {
-    lc.setRow(addr,0,pixels[0]);
-    lc.setRow(addr,1,pixels[1]);
-    lc.setRow(addr,2,pixels[2]);
-    lc.setRow(addr,3,pixels[3]);
-    lc.setRow(addr,4,pixels[4]);
-    lc.setRow(addr,5,pixels[5]);
-    lc.setRow(addr,6,pixels[6]);
-    lc.setRow(addr,7,pixels[7]);
-}
 
 static uint8_t hour2 = 12;
 static uint8_t minute2 = 0;
@@ -281,34 +83,9 @@ void loop() {
     uint8_t min16 = (minute & 0xF0) >> 4;
     uint8_t min1 = minute & 0x0F;
 
-    hourPixels = getDigit(hour);
-    minute16Pixels = getDigit(min16);
-    minute1Pixels = getDigit(min1);
-
-    draw(CS_MINUTE1, minute1Pixels);
-    draw(CS_MINUTE16, minute16Pixels);
-    draw(CS_HOUR, hourPixels);
+    disp.showDigit(CS_MINUTE1, min1);
+    disp.showDigit(CS_MINUTE16, min16);
+    disp.showDigit(CS_HOUR, hour);
 
     delay(500);
-}
-
-uint8_t* getDigit(uint8_t digit) {
-    switch (digit) {
-        case 0: return zero;
-        case 1: return one;
-        case 2: return two;
-        case 3: return three;
-        case 4: return four;
-        case 5: return five;
-        case 6: return six;
-        case 7: return seven;
-        case 8: return eight;
-        case 9: return nine;
-        case 10: return ten;
-        case 11: return eleven;
-        case 12: return twelve;
-        case 13: return thirteen;
-        case 14: return fourteen;
-        case 15: return fifteen;
-    }
 }
